5749e34ca32ea2f9bc6560e79aa16ea1295f122b
dlehd <dlehdrb5509@naver.com>
2025-05-12 20:28:41 +0900

    init spring project

---
+++ src/main/java/com/example/melopix/Melopix_backend/MelopixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.melopix.Melopix_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelopixBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelopixBackendApplication.class, args);
+	}
+
+}
